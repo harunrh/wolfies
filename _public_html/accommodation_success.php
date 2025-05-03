bbe9d19f40f4d85d68fc9aaa6c4599385a366c11
harunrh <159565823+harunrh@users.noreply.github.com>
2025-05-03 17:43:32 +0100

    Delete _public_html directory

--- _public_html/accommodation_success.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php';
-
-$loader = new \Twig\Loader\FilesystemLoader('OpenDay/templates');
-$twig = new \Twig\Environment($loader);
-
-echo $twig->render('accommodation_success.twig', [
-    'active_page' => 'university_life'
-]);
